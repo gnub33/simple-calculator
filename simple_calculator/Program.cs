49b588ed1aa920d113772d0e95b840862cf32c2b
dknight <dknight@code.wm.edu>
2025-09-13 21:48:35 -0400

    try attempt

--- simple_calculator/Program.cs
+++ simple_calculator/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome To Simple Calculator!");
+        Console.WriteLine("Welcome To Simple Calculator! Currently only accepting two numbers per calculation");
+
+        Console.WriteLine("Choose an operator: +, -, *, /");
+        string op = Console.ReadLine();
+
+        Console.WriteLine("Enter first number: ");
+        decimal firstNumber = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        decimal secondNumber = decimal.Parse(Console.ReadLine());
+        decimal result = 0;
+        Console.WriteLine();
+        Console.WriteLine($"{firstNumber} {op} {secondNumber} = {result}");
     }
 }
     
--- simple_calculator/obj/Debug/net9.0/simple_calculator.AssemblyInfo.cs
+++ simple_calculator/obj/Debug/net9.0/simple_calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simple_calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95939f1db8297a2df277aaad071ec14ace6ec599")]
 [assembly: System.Reflection.AssemblyProductAttribute("simple_calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simple_calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
